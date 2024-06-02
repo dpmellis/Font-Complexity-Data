66faad9fb1f9b78545903b86e60e960614e2326a
dpmellis <dpmellis@gmail.com>
2024-06-02 09:54:01 -0500

    Add files via upload

---
+++ Type Data Generator.py
@@ -0,0 +1,58 @@
+import drawBot
+import csv
+import time
+
+from importlib import reload
+import typeExtractor
+reload(typeExtractor)
+
+from typeExtractor import *
+
+log = open('./Log/log.txt','w') #initialize log file
+log.write('')
+log.close()
+
+exportData = [['Font Name', 'Family', 'Serif', 'Weight', 'Optical Size', 'Italic', 'Font Width', 'Style', 'Case', 'Glyph Name', 'Glyph Width', 'Glyph Height', 'Shape Width', 'Shape Height',  'Shape Area', 'On Curve Points', 'Total Points']] #Headings for the data, 
+        
+lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+testLetters = ['A','B','C']
+
+fileName = 'fonts' #The name of a csv file that contains the PostScript font name and typographic information about that font.
+#The fields are Font Name, Family, Weight, Optical Size, Italic, Font Width, Style    
+
+fontNames =[]
+with open(fileName+'.csv', newline='') as csvfile: #Opens filename.csv and loads it into fontNames
+    fontReader = csv.reader(csvfile, delimiter =',')
+    for name in fontReader:
+        if any(name): #ignore blank rows
+            fontNames.append(name)
+
+totalNames= len(fontNames)
+print(totalNames)
+
+#Extracts data from 10 rows of fontNames at a time. The position in fontNames is given by i. This is because as of now the program slows considerably after about extracting data from 17 fonts [something to fix]
+tic = time.perf_counter() 
+i = 0
+print(i)
+exportData=[]
+for currentFont in fontNames[i*10:(i+1)*10]: 
+    #Extract uppercase data
+    exportData = exportData + extractData(upperCase,currentFont + ['Upper'])
+    #Extract lowercase data
+    exportData = exportData + extractData(lowerCase,currentFont + ['Lower'])
+    #Extract data from testLetters
+#        exportData = exportData + extractData(testLetters, currentFont + ['test'])
+
+#Write the data in a directory called dataDump labeled with i
+with open('./dataDump/'+fileName+' output'+str(i)+'.csv', 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)     # creating a csv writer object
+    for rows in exportData:
+        csvwriter.writerow(rows) # writing the fields
+
+toc = time.perf_counter()
+print(f"Took {toc - tic:0.2f} seconds")
+
+
---
+++ typeExtractor.py
@@ -0,0 +1,61 @@
+from drawBot import *
+import csv
+
+
+def extractData(glyphSet, fontInfo, flag=1):
+    data = []
+
+    for letter in glyphSet:
+       
+        font(fontInfo[0],fontSize=1000)
+        #This is the width of the glyph including sidebearings, assuming a em size of 1000 units
+        widthBearing = int(textSize(letter)[0])
+        #This should be 1000, but isn't always
+        heightBearing = int(textSize(letter)[1])
+
+        #create a bezier curve of the letter with no sidebearings
+        currentLetter = BezierPath() 
+        currentLetter.text(letter, font=fontInfo[0], fontSize=1000) 
+
+        #This returns the width and height of the shape with no sidebearings and the total area. Area = 1 is a solid em-square 1000 units by 1000 units. If the flag is not 1, it skips the area calculation.
+        letterSizeData = letterSize(currentLetter, fontInfo[0], flag)            
+        
+        #Number of on curve points, no duplicates
+        onCurvePoints = len(set(currentLetter.onCurvePoints )) 
+        #Total number points, duplicates are allowed for off curve points, probably not likely
+        totalPoints = len(currentLetter.offCurvePoints) + onCurvePoints
+          
+        #Put everything together     
+        data.append(fontInfo + [letter, widthBearing, heightBearing, letterSizeData[0], letterSizeData[1], letterSizeData[2], onCurvePoints, totalPoints] )
+        
+    #Write a logfile of font names        
+    log = open('./Log/log.txt','a')
+    log.write(fontInfo[0]+'\n')
+    log.close()
+        
+    return data
+
+
+
+
+def letterSize(currentLetter, fontName, flag):
+    #Bounds of the shape with no sidebearings
+    (xmin, ymin,xmax, ymax) = currentLetter.bounds() 
+    shapeWidth = xmax-xmin #width of bounding box
+    shapeHeight = ymax-ymin #height of bounding box
+    currentLetter.translate(-xmin,-ymin) #Move bounding box of shape to the origin
+    
+    if flag ==1:
+        newPage(width = shapeWidth, height = shapeHeight)    #draw a new page that just fits the letter
+        font(fontName, fontSize=1000)
+        drawPath(currentLetter) #draw the letter in the page
+        saveImage('./Log/temp.tif') #save as a tif
+        letterGray = ImageObject('./Log/temp.tif') #load the tif
+        letterGray.areaAverage((0, 0, shapeWidth, shapeHeight)) #find the average color to calculate area
+        shapeArea = imagePixelColor(letterGray,(0, 0))[3] * shapeWidth * shapeHeight / 1000000 #Area =1 is a full em square
+    
+    else:
+        shapeArea=0 #may want to skip area calculation, which is slow
+    
+    newDrawing() #refresh the output window
+    return [int(shapeWidth), int(shapeHeight), shapeArea]
